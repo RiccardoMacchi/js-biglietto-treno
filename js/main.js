a9612d9eff847e99e95a0cf0d41c434b7009076d
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-04 16:41:52 +0200

    completata logica js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,29 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
+// Richiesta dati utente
+let priceKm = 0.21;
+let priceKmDisocunt;
+
+let numKmUser = parseInt(prompt("Quanti chilomenti vuoi percorrere?"));
+
+let ageUser = parseInt(prompt("Quanti anni hai?"));
+
+console.log("questi sono i chilomentri che l'utente vuole percorrere ", numKmUser);
+console.log("questa è l'età dell'utente", ageUser);
+
+// Condizione di età per sconto
+if (ageUser > 65){
+    priceKmDisocunt = (0.21 * 0.4)
+} else if (ageUser < 18){
+    priceKmDisocunt = (0.21 * 0.2)
+} else {
+    priceKmDisocunt = 0
+}
+
+console.log("questo è lo sconto al chilomentro", priceKmDisocunt);
+
+let ticketPrice = ((priceKm - priceKmDisocunt) * numKmUser);
+console.log(ticketPrice)
+console.log("Il prezzo per il biglietto è di", ticketPrice + "€")
+
